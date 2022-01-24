e307335a1c6f080056d6ea2d27f4a36b55f59afe
RythenGlyth <rythenglyth@gmail.com>
2022-01-24 18:58:54 +0100

    releaseartifact workflow

--- main.go
+++ main.go
@@ -7,48 +7,56 @@ import (
 )
 
 func main() {
-	var realArgs []string
-	for _, arg := range os.Args[1:] {
-		switch arg {
+	args := os.Args[1:]
+	var mediafolders []string
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
 		case "-h", "--help":
 
-			fmt.Println("Mediafire-Downloader")
+			fmt.Println("Usage: ", os.Args[0], " [OPTIONS]... [MEDIAFIREFOLDER]...")
+			fmt.Println()
+			fmt.Println("standard input is read as a batch file (see below)")
+			fmt.Println()
+			fmt.Println("MEDIAFIREFOLDER can be a folder or a file link")
 			fmt.Println()
 
 			fmt.Println("Options:")
-			fmt.Println("  -h, --help:          Show this help message")
-			fmt.Println()
-
-			fmt.Println("Arguments:")
-			fmt.Println("  FOLDERS:             Mediafire folder ID")
+			fmt.Println("  -h, --help:                  Show this help message")
+			fmt.Println("  -b, --batch [FILE]:          Download all folder and file links listed in FILE")
+			fmt.Println("  -o, --output [PATH]:         Set the output folder")
 
 			os.Exit(0)
 		case "-b", "--batch":
-
+			if i+1 < len(args) {
+				i++
+				file, err := os.Open(args[i])
+				if err != nil {
+					fmt.Println("Error: ", err)
+					os.Exit(1)
+				}
+				defer file.Close()
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					mediafolders = append(mediafolders, scanner.Text())
+				}
+			}
 		default:
-			realArgs = append(realArgs, arg)
+			mediafolders = append(mediafolders, args[i])
 		}
 	}
-
-	var f *os.File
-	var err error
 
 	if isInputFromPipe() {
-		f = os.Stdin
-	} else if len(realArgs) < 1 {
-		fmt.Println("Help: ", os.Args[0], "--help")
-		os.Exit(1)
-	} else {
-		f, err = os.Open(realArgs[0])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		f := os.Stdin
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			mediafolders = append(mediafolders, scanner.Text())
 		}
 	}
-
-	sc := bufio.NewScanner(f)
-
-	sc.Scan()
+	if len(mediafolders) < 1 {
+		fmt.Println("You need to specify at least one mediafire folder or file")
+		os.Exit(1)
+	}
+	fmt.Println("Downloading: ", mediafolders)
 
 }
 func isInputFromPipe() bool {
